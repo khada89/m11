7ce7631cb389ae0042dce5a8300e0dd3c3215db8
khada89 <castillomaximiliano89@gmail.com>
2026-02-10 00:20:41 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ValentinesApp from "./ValentinesApp.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ValentinesApp />
+  </React.StrictMode>
+);
